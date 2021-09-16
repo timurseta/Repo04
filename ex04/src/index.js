8a4ec5b49ee2ed310b25b943ce56d43effb3bb20
timurseta <timur_seta@hotmail.com>
2021-09-16 11:15:46 +0200

    added decimal points and scientific to code, added package.json

---
+++ ex04/src/index.js
@@ -0,0 +1,19 @@
+function main (){
+
+
+    // Only change code below this line
+
+    var myDecimal = 8.2;
+    var myDecimalE = 254e-5;
+
+
+    // Only change code above this line
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
