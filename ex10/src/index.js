340c852c886063e32307cc05a3b3d71bb0962aa2
timurseta <timur_seta@hotmail.com>
2021-09-16 14:43:30 +0200

    my info

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    // Only change code below this line
+
+    var myFirstName = "Timur"
+    var myLastName = "Seta"
+    var myHomeTown = "Sarajevo"
+
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
